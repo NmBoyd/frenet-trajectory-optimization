7657c5f521d002093cae5a3df49e0295fcc617d4
NmBoyd <nboyd2226@gmail.com>
2020-02-24 23:53:31 -0800

    deleting build

--- build/scripts/CMakeFiles/TestCubicSplines.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "TestCubicSplines.pdb"
-  "TestCubicSplines"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TestCubicSplines.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
